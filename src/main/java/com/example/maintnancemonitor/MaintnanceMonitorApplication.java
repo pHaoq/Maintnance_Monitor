48c2c90a5707da083ed1dd8dab6017efffb85f5a
Hao Lin <130253423+pHaoq@users.noreply.github.com>
2023-06-03 10:57:29 +0200

    first commit

---
+++ src/main/java/com/example/maintnancemonitor/MaintnanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintnancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintnanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintnanceMonitorApplication.class, args);
+    }
+
+}
