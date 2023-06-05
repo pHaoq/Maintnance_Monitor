ea8ef2946f8f0fb322719937a4ed799f5c35d483
MatthiasJeneW <matthias1.jenewein@gmail.com>
2023-06-05 20:15:24 +0200

    add 6 different tests

--- src/test/java/com/example/maintnancemonitor/MaintenanceControllerTest.java
+++ src/test/java/com/example/maintnancemonitor/MaintenanceControllerTest.java
@@ -7,21 +7,68 @@ import java.lang.reflect.Field;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaintenanceControllerTest {
+    private MaintenanceController maintenanceMonitor = new MaintenanceController();
+    String sMessage = "Everything operates as expected";
 
     @Test
     /**
      * checks the initial value of message
      */
-    public void checktinit(){
+    public void checktinit() {
 
         MaintenanceController controller = new MaintenanceController();
 
-        try{
+        try {
             Field message = MaintenanceController.class.getDeclaredField("message");
             message.setAccessible(true);
-            assertEquals(message.get(controller),"Everything works as expected","The initial message should be: Everything works as expected");
-        }catch (Exception e){
+            assertEquals(message.get(controller), "Everything works as expected", "The initial message should be: Everything works as expected");
+        } catch (Exception e) {
         }
     }
+
+    @Test
+    public void testMessage() {
+        String message = "New message";
+        maintenanceMonitor.setmessage(message);
+        assertEquals(maintenanceMonitor.message(), message);
+    }
+
+    @Test
+    public void testReset() {
+        String rMessage = "random message";
+        maintenanceMonitor.setmessage((rMessage));
+        maintenanceMonitor.reset();
+        assertEquals(sMessage, rMessage);
+
+    }
+
+    @Test
+    public void testUpdate() {
+        String iMessage = "initial Message";
+        maintenanceMonitor.setmessage(iMessage);
+        String uMessage = "update Message";
+        maintenanceMonitor.setmessage(uMessage);
+        assertEquals(uMessage, maintenanceMonitor.message());
+    }
+
+    @Test
+    public void testNull() {
+        String nMessage = null;
+        maintenanceMonitor.setmessage(nMessage);
+        assertEquals(sMessage, maintenanceMonitor.message());
+    }
+
+    @Test
+    public void testResetWithoutsetMessage() {
+        maintenanceMonitor.reset();
+        assertEquals(sMessage, maintenanceMonitor.message());
+    }
+
+    public void testRestWithsMessage() {
+        maintenanceMonitor.setmessage(sMessage);
+        maintenanceMonitor.reset();
+        assertEquals(sMessage, maintenanceMonitor.message());
+    }
+
 
 }
